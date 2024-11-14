47507a73f0f50af713f89fa4006d48d3f719f199
farouk ghezal <faroukghezal3@gmail.com>
2024-11-14 15:25:29 +0100

    project first step

---
+++ flask_app/controllers/category_controller.py
@@ -0,0 +1,23 @@
+from flask_app import app
+from flask import render_template, redirect, request, session, flash # type: ignore
+from flask_app.models.category_model import Category
+
+
+@app.route('/')
+def index():
+    return redirect('/admin')
+
+#dashboard route
+@app.route('/admin')
+def admin_dashboard():
+    all_categories=Category.get_all()
+    return render_template('admin_dashboard.html',all_categories=all_categories)
+
+#Action route to create category
+@app.route('/admin/create_category', methods = ['POST'])
+def create_category():
+    data={
+        **request.form
+    }
+    Category.create(data)
+    return redirect('/admin')
---
+++ flask_app/models/category_model.py
@@ -0,0 +1,26 @@
+from flask_app.config.mysqlconnections import connectToMySQL
+from flask_app import DB
+from flask import flash
+
+class Category :
+    def __init__(self, data):
+        self.id=data["id"]
+        self.category_name=data["category_name"]
+
+    @classmethod
+    def create(cls,data):
+        query = """INSERT INTO categories
+                    (category_name)
+                    VALUES (%(category_name)s);
+                """
+        return connectToMySQL(DB).query_db(query,data)
+    
+
+    @classmethod
+    def get_all(cls):
+        query="SELECT * FROM categories;"
+        results=connectToMySQL(DB).query_db(query)
+        all_categories=[]
+        for row in results:
+            all_categories.append(cls(row))
+        return all_categories
